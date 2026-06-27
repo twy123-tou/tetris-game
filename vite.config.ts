7692ee6be246cb73e365d831f107c5cb8de60b46
twy123-tou <2914644122@qq.com>
2026-06-27 11:16:28 +0800

    小游喜

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { createViteConfig } from '@vtj/cli';
+import { createDevTools } from '@vtj/pro/vite';
+import proxy from './proxy.config';
+export default createViteConfig({
+  proxy,
+  plugins: [
+    createDevTools({})
+  ]
+});
+
